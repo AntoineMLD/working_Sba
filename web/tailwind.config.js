0a364ec24a59af602bbc9c6f086e25d8ea664f9d
AntoineMLD <antoine.moulard@hotmail.com>
2024-03-12 11:19:17 +0100

    modif du css en tailwind

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,8 +5,28 @@ module.exports = {
 ],
 theme: {
   extend: {
-    
+    // Définition des couleurs personnalisées
+    colors: {
+      'navbar-bg': 'hsl(210, 50%, 15%)',
+      'navbar-text': 'hsl(210, 20%, 80%)',
+      'navbar-text-focus': 'hsl(203, 100%, 50%)',
+      'navbar-bg-contrast': 'hsl(210, 50%, 25%)',
+    },
+    // Autres ajustements de thème si nécessaire
+    spacing: {
+      '100': '25rem', // Exemple d'ajout d'une nouvelle valeur de spacing
+    },
+    borderRadius: {
+      'large': '12px',
+    },
+    fontFamily: {
+      'sans': ['Arial', 'Helvetica', 'sans-serif'],
+    },
+    boxShadow: {
+      'navbar': '0 2px 4px rgba(0, 0, 0, 0.15)',
+      'menu': '0 0 20px rgba(0, 0, 0, 0.3)',
+    },
   },
 },
 plugins: [],
-}
+};
